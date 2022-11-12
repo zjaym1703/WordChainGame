67894d21acec6bcc5d122b5c38f6e088508164f9
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2022-11-12 17:20:50 +0900

    게임방 수정

--- WordChainGame/src/WordChainGameClientRoomView.java
+++ WordChainGame/src/WordChainGameClientRoomView.java
@@ -22,8 +22,11 @@ import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 
 public class WordChainGameClientRoomView extends JFrame{
@@ -41,6 +44,9 @@ public class WordChainGameClientRoomView extends JFrame{
 	private ImageIcon supriseEmo = new ImageIcon("images/emoticon_suprise.png");
 	private ImageIcon sleepEmo = new ImageIcon("images/emoticon_sleep.png");
 	private ImageIcon AngryEmo = new ImageIcon("images/emoticon_angry.png");
+	private ImageIcon sendButtonBasicImage = new ImageIcon("images/sendButtonBasic.png");
+	private ImageIcon sendButtonEnteredImage = new ImageIcon("images/sendButtonEntered.png");
+	private JButton sendButton = new JButton(sendButtonBasicImage);
 	
 	private JTextField textField;
 	
@@ -87,20 +93,34 @@ public class WordChainGameClientRoomView extends JFrame{
 		contentPanel.setLayout(null);
 		
 		JScrollPane chattingScrollPane = new JScrollPane();
-		chattingScrollPane.setBounds(6, 448, 688, 178);
+		chattingScrollPane.setBounds(6, 443, 670, 178);
 		contentPanel.add(chattingScrollPane);
 		
 		JLabel lblNewLabel = new JLabel("채팅");
 		chattingScrollPane.setColumnHeaderView(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(6, 628, 556, 38);
+		textField.setBounds(6, 625, 590, 34);
 		contentPanel.add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("전송");
-		btnNewButton.setBounds(574, 628, 120, 35);
-		contentPanel.add(btnNewButton);
+		sendButton.setBorderPainted(false);
+		sendButton.setContentAreaFilled(false);
+		sendButton.setFocusPainted(false);
+		sendButton.setBounds(599, 622, sendButtonBasicImage.getIconWidth(), sendButtonBasicImage.getIconHeight());
+		sendButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) { // 마우스가 버튼 위로 올라갈 때
+				sendButton.setIcon(sendButtonEnteredImage);
+				sendButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // 마우스가 버튼 밖으로 나갈 때
+				sendButton.setIcon(sendButtonBasicImage);
+				sendButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
+		contentPanel.add(sendButton);
 		
 		JLabel roomNameLabel = new JLabel("RoomName");
 		roomNameLabel.setBounds(17, 21, 135, 16);
@@ -126,7 +146,7 @@ public class WordChainGameClientRoomView extends JFrame{
                 super.paintComponent(g);
             }
 		};
-		scorePanel.setBounds(78, 78, 417, 113);
+		scorePanel.setBounds(45, 78, 417, 113);
 		scorePanel.setLayout(null);
 		contentPanel.add(scorePanel);
 		
@@ -144,7 +164,7 @@ public class WordChainGameClientRoomView extends JFrame{
                 super.paintComponent(g);
             }
 		};
-		timerPanel.setBounds(545, 78, 120, 120);
+		timerPanel.setBounds(521, 78, 120, 120);
 		timerPanel.setLayout(null);
 		
 		JLabel scoreLabel = new JLabel("30");
@@ -161,30 +181,95 @@ public class WordChainGameClientRoomView extends JFrame{
 		JButton happyemoButton = new JButton();
 		happyemoButton.setIcon(happyEmo);
 		happyemoButton.setBounds(6, 406, 35, 35);
+		happyemoButton.setBorderPainted(false); // JButton의 border 없애기
+		happyemoButton.setContentAreaFilled(false); // JButton의 내용영역 채우기 안함
+		happyemoButton.setFocusPainted(false); // JButton이 선택 되었을 때 생기는 테두리 없애기
+		happyemoButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) { // 마우스가 버튼 위로 올라갈 때
+				happyemoButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // 마우스가 버튼 밖으로 나갈 때
+				happyemoButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
 		contentPanel.add(happyemoButton);
 		
 		loveEmo = imageSetSize(loveEmo,30,30);
 		JButton loveButton = new JButton();
 		loveButton.setIcon(loveEmo);
 		loveButton.setBounds(42, 406, 35, 35);
+		loveButton.setBorderPainted(false);
+		loveButton.setContentAreaFilled(false);
+		loveButton.setFocusPainted(false);
+		loveButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) { // 마우스가 버튼 위로 올라갈 때
+				loveButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // 마우스가 버튼 밖으로 나갈 때
+				loveButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
 		contentPanel.add(loveButton);
 		
 		supriseEmo = imageSetSize(supriseEmo,30,30);
 		JButton supriseButton = new JButton();
 		supriseButton.setIcon(supriseEmo);
 		supriseButton.setBounds(78, 406, 35, 35);
+		supriseButton.setBorderPainted(false);
+		supriseButton.setContentAreaFilled(false);
+		supriseButton.setFocusPainted(false);
+		supriseButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) { // 마우스가 버튼 위로 올라갈 때
+				supriseButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // 마우스가 버튼 밖으로 나갈 때
+				supriseButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
 		contentPanel.add(supriseButton);
 		
 		sleepEmo = imageSetSize(sleepEmo,30,30);
 		JButton sleepeButton = new JButton();
 		sleepeButton.setIcon(sleepEmo);
 		sleepeButton.setBounds(114, 406, 35, 35);
+		sleepeButton.setBorderPainted(false);
+		sleepeButton.setContentAreaFilled(false);
+		sleepeButton.setFocusPainted(false);
+		sleepeButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) { // 마우스가 버튼 위로 올라갈 때
+				sleepeButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // 마우스가 버튼 밖으로 나갈 때
+				sleepeButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
 		contentPanel.add(sleepeButton);
 		
 		AngryEmo = imageSetSize(AngryEmo,30,30);
 		JButton angryButton = new JButton();
 		angryButton.setIcon(AngryEmo);
 		angryButton.setBounds(150, 406, 35, 35);
+		angryButton.setBorderPainted(false);
+		angryButton.setContentAreaFilled(false);
+		angryButton.setFocusPainted(false);
+		angryButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) { // 마우스가 버튼 위로 올라갈 때
+				angryButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // 마우스가 버튼 밖으로 나갈 때
+				angryButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
 		contentPanel.add(angryButton);
 	}
 	
